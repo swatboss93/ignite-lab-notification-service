f11f407687a7cfb9cada4e94b3bd552a41ad4433
Wallison de Oliveira <wallison_93@hotmail.com>
2022-12-15 23:29:47 -0300

    Creates notifcation validator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { Body, Post } from '@nestjs/common/decorators';
 import { randomUUID } from 'crypto';
+import { CreateNotificationBody } from './create-notification-body';
 import { PrismaService } from './prisma.service';
 
 @Controller('notifications')
@@ -13,7 +14,7 @@ export class AppController {
   }
 
   @Post()
-  async create(@Body() body: any) {
+  async create(@Body() body: CreateNotificationBody) {
     const { recipientId, content, category } = body;
 
     await this.prisma.notification.create({
