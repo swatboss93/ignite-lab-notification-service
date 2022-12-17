808ce32a0632b9b5ce50bda9248df25495be8067
Wallison de Oliveira <wallison_93@hotmail.com>
2022-12-17 15:51:48 -0300

    Creates http module

---
+++ src/infra/http/http.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { SendNotification } from 'src/application/use-cases/send-notification';
+import { NotificationsController } from './controllers/notifications.controller';
+
+@Module({
+  controllers: [NotificationsController],
+  providers: [SendNotification],
+})
+export class HttpModule {}
