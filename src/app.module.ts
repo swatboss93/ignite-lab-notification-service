e87a0531161b34cbb08a82387bebb975bca9211f
Wallison de Oliveira <wallison_93@hotmail.com>
2022-12-17 17:04:12 -0300

    Refactors paths

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from './infra/database/database.module';
+import { DatabaseModule } from '@infra/database/database.module';
 
 @Module({
   imports: [DatabaseModule],
