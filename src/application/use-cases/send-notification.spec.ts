3357ebca35144585dcef41fce034889464dd14b4
Wallison de Oliveira <wallison_93@hotmail.com>
2022-12-17 15:08:58 -0300

    Uses repository to test persistence of notification

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,15 +1,25 @@
-import { SendNotification } from "./send-notification";
+import { Notification } from '../entities/notification';
+import { SendNotification } from './send-notification';
+
+const notifications: Notification[] = [];
+
+const notificationsRepository = {
+  async create(notification: Notification) {
+    notifications.push(notification);
+  }
+}
+
 
 describe('Send notification', () => {
   it('creates a notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationsRepository);
 
-    const { notification } = await sendNotification.execute({
+    await sendNotification.execute({
       recipientId: 'UUID',
       content: 'Valid content',
       category: 'category',
     });
 
-    expect(notification).toBeTruthy();
+    expect(notifications).toHaveLength(1);
   });
 });
