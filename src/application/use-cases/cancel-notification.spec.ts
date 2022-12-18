6769e0ed09c08d13b7ce7c4c8d4b5d3f6e6efcd6
Wallison de Oliveira <wallison_93@hotmail.com>
2022-12-18 15:16:55 -0300

    Creates cancel notification use case

---
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -0,0 +1,39 @@
+import { Content } from '@application/entities/content';
+import { Notification } from '@application/entities/notification';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { CancelNotification } from './cancel-notification';
+import { NotificationNotFound } from './errors/notification-not-found';
+
+describe('cancel notification', () => {
+  it('cancels a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    const notification = new Notification({
+      content: new Content('valid content'),
+      category: 'category',
+      recipientId: 'recipientId',
+    });
+
+    await notificationsRepository.create(notification);
+
+    await cancelNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications[0].canceledAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('does not cancel a non existing notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'invalid-id'
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
