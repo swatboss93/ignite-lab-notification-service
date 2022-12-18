c0dcd1b698baae6763d594ce4616ad41e4986ef8
Wallison de Oliveira <wallison_93@hotmail.com>
2022-12-18 16:50:09 -0300

    Creates unread notification use case

---
+++ src/application/use-cases/unread-notification.spec.ts
@@ -0,0 +1,32 @@
+import { makeNotification } from '@test/factories/notification-factory';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { UnreadNotification } from './unread-notification';
+import { NotificationNotFound } from './errors/notification-not-found';
+
+describe('Read notification', () => {
+  it('unreads a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const unreadNotification = new UnreadNotification(notificationsRepository);
+
+    const notification = makeNotification();
+
+    await notificationsRepository.create(notification);
+
+    await unreadNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications[0].readAt).toBeNull();
+  });
+
+  it('does not read a non existing notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const readNotification = new UnreadNotification(notificationsRepository);
+
+    expect(() => {
+      return readNotification.execute({
+        notificationId: 'invalid-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
