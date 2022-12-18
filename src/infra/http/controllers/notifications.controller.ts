fa839a56d238347dac4af79cfb0e2af7ab6157ef
Wallison de Oliveira <wallison_93@hotmail.com>
2022-12-18 17:34:24 -0300

    Creates unread action in notifications controller

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -4,12 +4,14 @@ import { CreateNotificationBody } from '../dtos/create-notification-body';
 import { NotificationViewModel } from '../view-models/notification-view-model';
 import { CancelNotification } from '@application/use-cases/cancel-notification';
 import { ReadNotification } from '@application/use-cases/read-notification';
+import { UnreadNotification } from '@application/use-cases/unread-notification';
 
 @Controller('notifications')
 export class NotificationsController {
   constructor(
     private sendNotification: SendNotification,
     private readNotification: ReadNotification,
+    private unreadNotification: UnreadNotification,
     private cancelNotification: CancelNotification,
   ) {}
 
@@ -29,6 +31,11 @@ export class NotificationsController {
   @Patch(':id/read')
   async read(@Param('id') id: string) {
     await this.readNotification.execute({ notificationId: id });
+  }
+
+  @Patch(':id/unread')
+  async unread(@Param('id') id: string) {
+    await this.unreadNotification.execute({ notificationId: id });
   }
 
   @Patch(':id/cancel')
